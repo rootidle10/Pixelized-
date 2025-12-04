ea2f691b3bdfd2e016568727112e8f62f712291a
Pierre INTHAVONG <pi054954@iut21.u-bourgogne.fr>
2025-12-04 16:54:37 +0100

    MAJ .env.example + Route API

--- back/routes/api.php
+++ back/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Sudoku\SudokuController;
 
 // Routes publiques
 Route::post('/register', [AuthController::class, 'register']);
@@ -13,4 +14,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:sanctum')->prefix('sudoku')->group(function() {
+    Route::post('/new', [SudokuController::class, 'new']);
+    Route::post('/validate', [SudokuController::class, 'validateGrid']);
+    Route::post('/solve/{id}', [SudokuController::class, 'solve']);
 });
